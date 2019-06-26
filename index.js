bd3414a4d025b6a97765a37c98c73977d2cf1207
elbukaevzaur <elbukaevzaur@gmail.com>
2019-06-26 05:09:17 +0300

    response if data

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const Telegraf = require('telegraf')
- 
-const bot = new Telegraf('823203998:AAGOTjIzNdvPHFpDTn_v3jdTJuLxhjgFJHg')
-bot.start((ctx) => {
-    console.log('Id пользователя:', ctx.from.id);
-    return ctx.reply('Добро пожаловать!');
-  });
+
+const config = require('./config.json')
+const TelegramBot = require('node-telegram-bot-api');
+const token = config.token;
+
+const bot = new TelegramBot(token, { polling: true });
+
+var answerCallbacks = {};
+
+bot.on('message', function (message) {
+    var callback = answerCallbacks[message.chat.id];
+    if (callback) {
+        delete answerCallbacks[message.chat.id];
+        return callback(message);
+    }
+});
+
+bot.onText(/search/, function (message,match) {
+    bot.sendMessage(message.chat.id, "Введите ключевое слово").then(function () {
+        answerCallbacks[message.chat.id] = function (answer) {
+            var name = answer.text;
+            var text = "";
+            if(name == "by"){
+                text = "good by"
+            }else{
+                text = "Ошибка запроса"
+            }
+            bot.sendMessage(message.chat.id, text);
+        }
+    });
+});
+
